2d3f666b00d39fb7b58259ce005dce4b00be32d1
Phil Wigner <moldymoose@protonmail.com>
2024-10-17 15:31:02 -0400

    Created basic socket connection to print string from client on server

--- src/Client/ClientProgram.java
+++ src/Client/ClientProgram.java
@@ -1,7 +1,24 @@
 package Client;
 
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
 public class ClientProgram {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        try (Socket socket = new Socket("localhost", 6942)) {
+            OutputStream output = socket.getOutputStream();
+            PrintWriter writer = new PrintWriter(output, true);
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter a string: ");
+            String message = scanner.nextLine();
+
+            writer.println(message);
+
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/Server/ServerProgram.java
+++ src/Server/ServerProgram.java
@@ -1,7 +1,27 @@
 package Server;
 
+import java.io.*;
+import java.net.*;
+
 public class ServerProgram {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        try (ServerSocket serverSocket = new ServerSocket(6942)) {
+
+            while (true) {
+                Socket socket = serverSocket.accept();
+                InputStream input = socket.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
+                String message = reader.readLine();
+
+                System.out.println("Recieved from client:" + message);
+
+                socket.close();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
